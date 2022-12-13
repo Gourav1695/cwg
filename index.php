e5bdd0eb1d0c5b859a7d50e166b769300a9e518f
Gourav <gkumars2956@gmail.com>
2022-12-13 13:28:34 +0530

    Started learning PHP for Backend Development

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class= "container">
+        This is my First PHP project to learn about backend developments.
+        
+    <?php
+    // This is single line comment
+    /*
+    this is 
+    multiline
+    comment
+    */
+    
+    $variable1= 23;
+    $variable2 = 45;
+    echo $variable1;
+
+    Echo $variable1 + $variable2;
+     echo "Hello World this is written by using the PHP language";
+     ?>
+</div>
+</body>
+</html>
